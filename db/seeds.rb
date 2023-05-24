bc972957c29303b247957ef200d10835a2e87f72
Jessica Roman <jessicaroman.mailpro@gmail.com>
2023-05-24 12:35:55 +0200

    reviews done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+restaurants = [
+  {
+  name: "mecs au camion",
+  address: "toulouse",
+  phone_number: "0561345678",
+  category: "chinese"
+  },
+  {
+    name: "velo végé",
+    address: "toulouse",
+    phone_number: "0561340987",
+    category: "italian"
+  },
+  {
+    name: "bahn mi",
+    address: "toulouse",
+    phone_number: "0561345634",
+    category: "japanese"
+  },
+  {
+    name: "Ali 2000",
+    address: "toulouse",
+    phone_number: "0561349078",
+    category: "french"
+  },
+  {
+    name: "India love",
+    address: "toulouse",
+    phone_number: "0561345123",
+    category: "belgian"
+  }
+]
+# reviews = [
+#   {
+#     content: [
+#       "très bon resto",
+#       "super!!",
+#       "a chier!!"
+#     ]
+#   }
+# ]
+
+puts "Creating restaurants..."
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+# reviews.each do |attributes|
+#   review = Review.create!(attributes)
+#   puts "Created review"
+# end
+
+puts "Finished!"
